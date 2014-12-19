d54c58fd70ca126acf16d30c53aee304b758a343
kevin-ye <kevin.ye32@gmail.com>
2014-12-19 15:16:48 -0500

    Scenario

--- KerbalCooperation/KerbalCooperation.cs
+++ KerbalCooperation/KerbalCooperation.cs
@@ -4,11 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using KSP;
 
-namespace KerbalCooperation
+namespace KCoop
 {
-    public class KerbalCooperation_control
+    [KSPAddon(KSPAddon.Startup.SpaceCentre,false)]
+    public class AddScenarioModules : MonoBehaviour
     {
+        void Start()
+        {
+            var game = HighLogic.CurrentGame;
+
+            ProtoScenarioModule psm = game.scenarios.Find(s => s.moduleName == typeof(KerbalCooperation).Name);
+            if (psm == null)
+            {
+                Logger.log("Adding the scenario module.");
+                psm = game.AddProtoScenarioModule(typeof(KerbalCooperation), GameScenes.SPACECENTER,
+                    GameScenes.FLIGHT, GameScenes.EDITOR);
+            }
+            else
+            {
+                if (!psm.targetScenes.Any(s => s == GameScenes.SPACECENTER))
+                {
+                    psm.targetScenes.Add(GameScenes.SPACECENTER);
+                }
+                if (!psm.targetScenes.Any(s => s == GameScenes.FLIGHT))
+                {
+                    psm.targetScenes.Add(GameScenes.FLIGHT);
+                }
+                if (!psm.targetScenes.Any(s => s == GameScenes.EDITOR))
+                {
+                    psm.targetScenes.Add(GameScenes.EDITOR);
+                }
+            }
+        }
+    }
+
+    public class KerbalCooperation : ScenarioModule
+    {
+        private static KerbalCooperation Instance { get; private set;}
+        public KerbalCooperation()
+        {
+            Logger.log("Scenario constructor.");
+            Instance = this;
+        }
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+        }
     }
 }
