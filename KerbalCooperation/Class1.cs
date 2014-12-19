cc44e4ce60500051eaad6c135d83de8150da542a
kevin-ye <kevin.ye32@gmail.com>
2014-12-19 14:45:41 -0500

    init

---
+++ KerbalCooperation/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using KSP;
+
+namespace KerbalCooperation
+{
+    public class KerbalCooperation_control
+    {
+    }
+}
