3368ead553355893565ef1ea3ee73d5a48a614a5
kevin-ye <kevin.ye32@gmail.com>
2015-01-08 07:55:10 -0500

    all

---
+++ KerbalCooperation/KCoopSpaceCenterWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using window = ReeperCommon.Window;
+
+namespace KCoop
+{
+    class KCoopSpaceCenterWindow: window.DraggableWindow
+    {
+        private SpaceCenterManager parent = null;
+        protected override Rect Setup()
+        {
+            Logger.log("SpaceCenterWindow setup.");
+
+            return new Rect(300, 300, 300, 300);
+        }
+
+        protected override void DrawUI()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("test SpaceCenterManager.DrawUI");
+            GUILayout.EndVertical();
+        }
+
+        protected override void OnCloseClick()
+        {
+            Logger.log("SpaceCenterWindow OnCloseClick.");
+        }
+
+        public void setParent(SpaceCenterManager manager)
+        {
+            parent = manager;
+        }
+
+        public void notify(notifyInfo info)
+        {
+
+        }
+    }
+}
---
+++ KerbalCooperation/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace KCoop
+{
+    // UnityEngine debug log warpper
+    class Logger
+    {
+        private const string fronter = "KCoop: ";
+        public static void log(string info)
+        {
+            Debug.Log(fronter + info);
+        }
+
+        public static void error(string info)
+        {
+            Debug.LogError(fronter + info);
+        }
+
+        public static void exception(string info)
+        {
+            Debug.LogException(new Exception(fronter + info));
+        }
+
+        public static void warnning(string info)
+        {
+            Debug.LogWarning(fronter + info);
+        }
+    }
+}
